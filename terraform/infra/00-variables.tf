d4adf7a5260ed963e65e17d0f7c11dd72b8043b5
Ray Kao <ray@peopleandcode.com>
2021-09-23 01:54:29 -0400

    keeping file convention consistent

---
+++ terraform/infra/00-variables.tf
@@ -0,0 +1,21 @@
+variable "prefix" {
+  type = string
+  default = "rk"
+}
+
+variable "subId1" {
+  type = string
+}
+
+variable "subId2" {
+  type = string
+}
+
+variable "global_address_space" {
+  type = string
+  default = "10.0.0.0/8"
+}
+
+variable "domain" {
+  type = string
+}
--- terraform/infra/01-main.tf
+++ terraform/infra/01-main.tf
@@ -19,28 +19,6 @@ provider azurerm {
 	features {}
 }
 
-variable "prefix" {
-  type = string
-  default = "rk"
-}
-
-variable "subId1" {
-  type = string
-}
-
-variable "subId2" {
-  type = string
-}
-
-variable "global_address_space" {
-  type = string
-  default = "10.0.0.0/8"
-}
-
-variable "domain" {
-  type = string
-}
-
 locals {
   prefix = "${var.prefix}demoenv"
   location = "eastus"
