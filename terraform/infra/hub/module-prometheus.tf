c759f410a8fa6c2150ddf9856c82dbdaa17e93c9
Ray Kao <ray@peopleandcode.com>
2021-10-07 22:10:21 -0400

    attempting initial prometheus cluster setup

---
+++ terraform/infra/hub/module-prometheus.tf
@@ -0,0 +1,10 @@
+# module "prometheus" {
+# 	source = "../../modules/prometheus"
+# 	user_msi = var.contributor_msi.id
+# 	prefix = local.prefix
+# 	resource_group = azurerm_resource_group.hub
+# 	subnet_id = azurerm_subnet.prometheus.id
+# 	admin_username = var.admin_username
+# 	ssh_key = var.ssh_key
+# 	vm_size = "Standard_F4s_v2"
+# }
