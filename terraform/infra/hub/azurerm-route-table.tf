45134ea0f70b90e63295d66d94f29ef9a6700dd4
Ray Kao <ray@peopleandcode.com>
2021-10-28 09:57:20 -0400

    route tables are not required now that traffic is forced to vwan

--- terraform/infra/hub/azurerm-route-table.tf
+++ terraform/infra/hub/azurerm-route-table.tf
@@ -1,18 +1,15 @@
-resource "azurerm_route_table" "default" {
-  depends_on = [
-    # azurerm_virtual_network_gateway.default
-  ]
-  name                = "default-routetable"
-  location            = azurerm_resource_group.hub.location
-  resource_group_name = azurerm_resource_group.hub.name
+# resource "azurerm_route_table" "default" {
+#   name                = "default-routetable"
+#   location            = azurerm_resource_group.hub.location
+#   resource_group_name = azurerm_resource_group.hub.name
 
-  route {
-    name                   = "hub"
-    address_prefix         = "0.0.0.0/0"
-    next_hop_type          = "VirtualAppliance"
-    next_hop_in_ip_address = azurerm_firewall.hub.ip_configuration[0].private_ip_address
-  }
-}
+#   route {
+#     name                   = "hub"
+#     address_prefix         = "0.0.0.0/0"
+#     next_hop_type          = "VirtualAppliance"
+#     next_hop_in_ip_address = azurerm_firewall.hub.ip_configuration[0].private_ip_address
+#   }
+# }
 
 # resource "azurerm_subnet_route_table_association" "consul" {
 #   subnet_id      = azurerm_subnet.consul.id
