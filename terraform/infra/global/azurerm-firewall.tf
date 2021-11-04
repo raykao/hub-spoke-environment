7c8bff2304597fb0914619df1fbc5a45deb0c0d5
Ray Kao <ray@peopleandcode.com>
2021-11-03 23:29:07 -0400

    allow ability to remote to a jumpbox via firewall dnat rule

--- terraform/infra/global/azurerm-firewall.tf
+++ terraform/infra/global/azurerm-firewall.tf
@@ -62,3 +62,89 @@ resource "azurerm_firewall_policy_rule_collection_group" "canadacentral" {
     }
   }
 }
+
+
+resource "azurerm_firewall_policy_rule_collection_group" "spoke1" {
+	name               = "spoke1-fwpolicy-rcg"
+  firewall_policy_id = azurerm_firewall_policy.canadacentral.id
+  priority           = 100
+
+nat_rule_collection {
+    name     = "jumpbox-ssh"
+    priority = 300
+    action   = "Dnat"
+    rule {
+      name                = "ssh"
+      protocols           = ["TCP"]
+      source_addresses    = ["*"]
+      destination_address = azurerm_firewall.canadacentral.virtual_hub[0].public_ip_addresses[0]
+      destination_ports   = ["2022"]
+      translated_address  = "10.1.255.4"
+      translated_port     = "2022"
+    }
+  }
+
+	nat_rule_collection {
+    name     = "jumpbox-xrdp"
+    priority = 301
+    action   = "Dnat"
+    rule {
+      name                = "xrdp"
+      protocols           = ["TCP"]
+      source_addresses    = ["*"]
+      destination_address = azurerm_firewall.canadacentral.virtual_hub[0].public_ip_addresses[0]
+      destination_ports   = ["2023"]
+      translated_address  = "10.1.255.4"
+      translated_port     = "2023"
+    }
+  }
+
+	network_rule_collection {
+		name = "jumpbox-allow-all-outbound"
+		priority = 400
+		action = "Allow"
+
+		rule {
+			name = "all"
+			protocols = ["Any"]
+			source_addresses = ["10.1.255.0/24"]
+			destination_ports = ["*"]
+			destination_addresses = ["*"]
+		} 
+	}
+
+	application_rule_collection {
+    name     = "ubuntuUpdates"
+    priority = 500
+    action   = "Allow"
+
+    rule {
+      name = "app_rule_collection1_rule1"
+      
+			protocols {
+        type = "Http"
+        port = 80
+      }
+
+      protocols {
+        type = "Https"
+        port = 443
+      }
+      
+			source_addresses  = [
+				"10.1.0.0/16"
+			]
+      
+			destination_fqdns = [
+				"*.ubuntu.com",
+				"*.archive.ubuntu.com",
+				"*.azure.archive.ubuntu.com",
+				"apt.releases.hashicorp.com",
+				"*.microsoft.com",
+				"aka.ms",
+				"management.azure.com"
+			]
+    }
+  }
+}
+
