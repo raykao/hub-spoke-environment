8c77a4623ee21bb626d454108173d3481e13fd1f
Ray Kao <ray@peopleandcode.com>
2021-11-04 16:10:33 -0400

    remove spoke3 jumpbox - not needed

--- terraform/infra/spoke3/02-outputs.tf
+++ terraform/infra/spoke3/02-outputs.tf
@@ -6,14 +6,6 @@ output resource_group {
 	value = azurerm_resource_group.spoke3
 }
 
-output jumpbox {
-	value = {
-		ip_address 	= module.jumpbox.ip
-		fqdn				= module.jumpbox.fqdn
-		ssh					= "ssh -p 2022 ${var.admin_username}@${module.jumpbox.ip}"
-	}
-}
-
 output public_dns_zone {
 	value = azurerm_dns_zone.spoke3
 }
--- terraform/infra/spoke3/module-jumpbox.tf
+++ terraform/infra/spoke3/module-jumpbox.tf
@@ -1,10 +1,10 @@
-module "jumpbox" {
-	resource_group = azurerm_resource_group.spoke3
+# module "jumpbox" {
+# 	resource_group = azurerm_resource_group.spoke3
 	
-	source = "../../modules/jumpbox"
-  prefix = local.prefix
-	subnet_id = azurerm_subnet.jumpbox.id
-	admin_username = var.admin_username
-	ssh_key = var.ssh_key
-	admin_email = var.admin_email
-}
+# 	source = "../../modules/jumpbox"
+#   prefix = local.prefix
+# 	subnet_id = azurerm_subnet.jumpbox.id
+# 	admin_username = var.admin_username
+# 	ssh_key = var.ssh_key
+# 	admin_email = var.admin_email
+# }
