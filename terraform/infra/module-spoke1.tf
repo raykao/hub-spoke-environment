556c12213fdfbb2efc3f6600eb6047e39918cd16
Ray Kao <ray@peopleandcode.com>
2022-04-14 18:44:19 -0400

    temp: spokes not currently needed

--- terraform/infra/module-spoke1.tf
+++ terraform/infra/module-spoke1.tf
@@ -1,38 +1,38 @@
-module spoke1 {
-	source = "./spoke1"
+# module spoke1 {
+# 	source = "./spoke1"
 	
-	providers = {
-		azurerm = azurerm.sub1
-	}
-	prefix = "${local.prefix}s1"
-	location = "canadacentral"
-	contributor_msi = module.global.contributor_msi
-	admin_username = local.admin_username
-	address_space = cidrsubnet(var.global_address_space, 8, 1)
-	domain = var.domain
-	hub = {
-		vnet = module.hub.vnet
-	}
-	admin_groups = var.admin_groups
-	admin_email = var.admin_email
-	ssh_key = module.global.public_key
+# 	providers = {
+# 		azurerm = azurerm.sub1
+# 	}
+# 	prefix = "${local.prefix}s1"
+# 	location = "canadacentral"
+# 	contributor_msi = module.global.contributor_msi
+# 	admin_username = local.admin_username
+# 	address_space = cidrsubnet(var.global_address_space, 8, 1)
+# 	domain = var.domain
+# 	hub = {
+# 		vnet = module.hub.vnet
+# 	}
+# 	admin_groups = var.admin_groups
+# 	admin_email = var.admin_email
+# 	ssh_key = module.global.public_key
 
-}
+# }
 
-resource "azurerm_virtual_hub_connection" "spoke1" {
-	provider = azurerm.sub1
-  name                      = "canadacentral-spoke1-vhub"
-  virtual_hub_id            = module.global.virtual_hubs["canadacentral"].id
-  remote_virtual_network_id = module.spoke1.vnet.id
-	internet_security_enabled = true
-}
+# resource "azurerm_virtual_hub_connection" "spoke1" {
+# 	provider = azurerm.sub1
+#   name                      = "canadacentral-spoke1-vhub"
+#   virtual_hub_id            = module.global.virtual_hubs["canadacentral"].id
+#   remote_virtual_network_id = module.spoke1.vnet.id
+# 	internet_security_enabled = true
+# }
 
-resource "azurerm_dns_ns_record" "spoke1" {
-  provider = azurerm.sub1
-  name = "spoke1"
-  zone_name           = module.global.public_dns_zone.name
-  resource_group_name = module.global.public_dns_zone.resource_group_name
-  ttl                 = 30
+# resource "azurerm_dns_ns_record" "spoke1" {
+#   provider = azurerm.sub1
+#   name = "spoke1"
+#   zone_name           = module.global.public_dns_zone.name
+#   resource_group_name = module.global.public_dns_zone.resource_group_name
+#   ttl                 = 30
 
-  records = module.spoke1.public_dns_zone.name_servers
-}
+#   records = module.spoke1.public_dns_zone.name_servers
+# }
--- terraform/infra/module-spoke2.tf
+++ terraform/infra/module-spoke2.tf
@@ -1,38 +1,38 @@
-module spoke2 {
-	source = "./spoke2"
+# module spoke2 {
+# 	source = "./spoke2"
 	
-	providers = {
-		azurerm = azurerm.sub2
-	}
-	prefix = "${local.prefix}s2"
-	location = "canadacentral"
-	contributor_msi = module.global.contributor_msi
-	admin_username = local.admin_username
-	address_space = cidrsubnet(var.global_address_space, 8, 2)
-	domain = var.domain
-	priv-dns-zone-links = {
-		hub = module.hub.vnet.id
-		spoke1 = module.spoke1.vnet.id
-	}
-	hub = {
-		vnet = module.hub.vnet
-	}
-}
+# 	providers = {
+# 		azurerm = azurerm.sub2
+# 	}
+# 	prefix = "${local.prefix}s2"
+# 	location = "canadacentral"
+# 	contributor_msi = module.global.contributor_msi
+# 	admin_username = local.admin_username
+# 	address_space = cidrsubnet(var.global_address_space, 8, 2)
+# 	domain = var.domain
+# 	priv-dns-zone-links = {
+# 		hub = module.hub.vnet.id
+# 		spoke1 = module.spoke1.vnet.id
+# 	}
+# 	hub = {
+# 		vnet = module.hub.vnet
+# 	}
+# }
 
-resource "azurerm_virtual_hub_connection" "spoke2" {
-	provider = azurerm.sub1
-  name                      = "canadacentral-spoke2-vhub"
-  virtual_hub_id            = module.global.virtual_hubs["canadacentral"].id
-  remote_virtual_network_id = module.spoke2.vnet.id
-	internet_security_enabled = true
-}
+# resource "azurerm_virtual_hub_connection" "spoke2" {
+# 	provider = azurerm.sub1
+#   name                      = "canadacentral-spoke2-vhub"
+#   virtual_hub_id            = module.global.virtual_hubs["canadacentral"].id
+#   remote_virtual_network_id = module.spoke2.vnet.id
+# 	internet_security_enabled = true
+# }
 
-resource "azurerm_dns_ns_record" "spoke2" {
-  provider = azurerm.sub1
-  name = "spoke2"
-  zone_name           = module.global.public_dns_zone.name
-  resource_group_name = module.global.public_dns_zone.resource_group_name
-  ttl                 = 30
+# resource "azurerm_dns_ns_record" "spoke2" {
+#   provider = azurerm.sub1
+#   name = "spoke2"
+#   zone_name           = module.global.public_dns_zone.name
+#   resource_group_name = module.global.public_dns_zone.resource_group_name
+#   ttl                 = 30
 
-  records = module.spoke2.public_dns_zone.name_servers
-}
+#   records = module.spoke2.public_dns_zone.name_servers
+# }
