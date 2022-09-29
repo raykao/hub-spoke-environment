624f05cfc59547a78b7fdf00b181fd7b73fe6fca
Ray Kao <raykao@gmail.com>
2022-09-29 17:42:14 -0400

    fix: missing pkcs12 provider and better method to assign subnets

--- terraform/infra/01-main.tf
+++ terraform/infra/01-main.tf
@@ -4,6 +4,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.23.0"
     }
+	pkcs12 = {
+      source = "chilicat/pkcs12"
+      version = "0.0.7"
+    }
   }
 }
 
@@ -19,17 +23,20 @@ provider azurerm {
 	features {}
 }
 
+provider "pkcs12" {
+  # Configuration options
+}
+
 locals {
 	# global = data.terraform_remote_state.global.outputs
-  prefix = var.prefix
-  location = "eastus"
-  admin_username = var.admin_username
-  cidrs = {
-	hub = cidrsubnet(var.global_address_space, 8, 255)
-	onprem = cidrsubnet(var.global_address_space, 16, 0)
-	spoke1 = cidrsubnet(var.global_address_space, 8, 1)
-	spoke2 = cidrsubnet(var.global_address_space, 8, 2)
-	spoke3 = cidrsubnet(var.global_address_space, 8, 3)
-	spoke4 = cidrsubnet(var.global_address_space, 8, 4)
-  }
+	prefix = var.prefix
+	location = "eastus"
+	admin_username = var.admin_username
+	hub_cidr = cidrsubnet(var.global_address_space, 8, 255)
+	jumpbox_cidr = cidrsubnet(local.hub_cidr, 8, 255)
+	onprem_cidr = cidrsubnet(var.global_address_space, 16, 0)
+	spoke1_cidr = cidrsubnet(var.global_address_space, 8, 1)
+	spoke2_cidr = cidrsubnet(var.global_address_space, 8, 2)
+	spoke3_cidr = cidrsubnet(var.global_address_space, 8, 3)
+	spoke4_cidr = cidrsubnet(var.global_address_space, 8, 4)
 }
