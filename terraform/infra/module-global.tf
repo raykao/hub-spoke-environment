e027a1a3251b2fc2011e23c28885840ea7a4e4df
Ray Kao <raykao@gmail.com>
2022-09-29 17:43:15 -0400

    fix: need to explicitly pass in provider and updated cidr values

--- terraform/infra/module-global.tf
+++ terraform/infra/module-global.tf
@@ -3,10 +3,12 @@ module global {
 	
 	providers = {
 		azurerm = azurerm.sub1
+		pkcs12 = pkcs12
 	}
 	prefix = local.prefix
 	location = "canadacentral"
 	domain = var.domain
 	admin_email = var.admin_email
-	onprem_cidr = local.cidrs.onprem
+	onprem_cidr = local.onprem_cidr
+	jumpbox_subnet_cidr = local.jumpbox_cidr
 }
