5ae8cc30a5b81bdf119098925ee297b0e66d19ce
Ray Kao <raykao@gmail.com>
2022-09-29 23:22:14 -0400

    fix: make subnet sizes easier to manage

--- terraform/infra/hub/azurerm-virtual-network.tf
+++ terraform/infra/hub/azurerm-virtual-network.tf
@@ -3,33 +3,27 @@ resource "azurerm_virtual_network" "hub" {
 	resource_group_name = azurerm_resource_group.hub.name
 	location = azurerm_resource_group.hub.location
 	address_space       = [var.address_space]
-	# dns_servers = [
-	# 	cidrhost(local.bind9_address_space, 4),
-	# 	cidrhost(local.bind9_address_space, 5),
-	# 	cidrhost(local.bind9_address_space, 6),
-	# 	"168.63.129.16"
-	# ]
 }
 
 resource azurerm_subnet consul {
   name = "ConsulSubnet"
   resource_group_name =  azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes = [cidrsubnet(var.address_space, 13, 8)]
+  address_prefixes = [cidrsubnet(var.address_space, 8, 1)]
 }
 
 resource azurerm_subnet nomad {
   name = "NomadSubnet"
   resource_group_name =  azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes = [cidrsubnet(var.address_space, 13, 9)]
+  address_prefixes = [cidrsubnet(var.address_space, 8, 2)]
 }
 
 resource azurerm_subnet vault {
   name = "VaultSubnet"
   resource_group_name =  azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes = [cidrsubnet(var.address_space, 12, 7)]
+  address_prefixes = [cidrsubnet(var.address_space, 8, 3)]
 	service_endpoints    = ["Microsoft.Storage"]
 	private_endpoint_network_policies_enabled = true
 }
@@ -38,23 +32,23 @@ resource azurerm_subnet pki {
   name = "PkiSubnet"
   resource_group_name =  azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes = [cidrsubnet(var.address_space, 12, 8)]
+  address_prefixes = [cidrsubnet(var.address_space, 8, 4)]
 	service_endpoints    = ["Microsoft.Storage"]
 	private_endpoint_network_policies_enabled = true
 }
 
-# resource azurerm_subnet prometheus {
-#   name = "PrometheusSubnet"
-#   resource_group_name =  azurerm_resource_group.hub.name
-#   virtual_network_name = azurerm_virtual_network.hub.name
-#   address_prefixes = [cidrsubnet(var.address_space, 12, )]
-# }
-
-resource azurerm_subnet dns {
-  name = "DnsSubnet"
+resource azurerm_subnet prometheus {
+  name = "PrometheusSubnet"
   resource_group_name =  azurerm_resource_group.hub.name
   virtual_network_name = azurerm_virtual_network.hub.name
-  address_prefixes = [local.bind9_address_space]
+  address_prefixes = [cidrsubnet(var.address_space, 8, 5)]
+}
+
+resource "azurerm_subnet" "aca-infra" {
+  name = "AcaInfraSubnet"
+  resource_group_name = azurerm_resource_group.hub.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes = [cidrsubnet(var.address_space, 8, 6)]
 }
 
 resource azurerm_subnet jumpbox {
