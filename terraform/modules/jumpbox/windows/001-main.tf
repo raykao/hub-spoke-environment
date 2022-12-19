9d31bc9aa7d8bd74b5d16fd9b28e6feb8dd14776
Ray Kao <raykao@gmail.com>
2022-12-19 11:27:12 -0500

    fix: running into windows vm hostname character limit

--- terraform/modules/jumpbox/windows/001-main.tf
+++ terraform/modules/jumpbox/windows/001-main.tf
@@ -7,8 +7,16 @@ resource "random_password" "admin_password" {
 }
 
 
+resource "random_string" "suffix" {
+	length  = 4
+	special = false
+	numeric 	= false
+	upper 	= false
+}
+
 locals {
   prefix    = var.prefix
-  hostname = "${local.prefix}win11"
+  suffix = random_string.suffix.result
+  hostname = "${local.prefix}win${local.suffix}"
   admin_password = var.admin_password != "" ? var.admin_password : random_password.admin_password.result
 }
